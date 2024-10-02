3865b5db8bfd29c3788ac91015cb3849db152a6f
Rafi Putra Sudirman <rafikujra@gmail.com>
2024-10-02 15:31:53 +0700

    fix dashboard dan penambahan material react table

---
+++ src/components/Tables/Dashboard/tableSurat.tsx
@@ -0,0 +1,168 @@
+import { useMemo } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_Row,
+  type MRT_ColumnDef,
+  createMRTColumnHelper,
+} from "material-react-table";
+import { Box, Button } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+
+const columnHelper = createMRTColumnHelper<Person>();
+
+//example data type
+type Person = {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  address: string;
+  city: string;
+  state: string;
+  nosp: string;
+  pensiun: string;
+  kerja: string;
+};
+
+//nested data is ok, see accessorKeys in ColumnDef below
+const data: Person[] = [
+  {
+    name: {
+      firstName: "098579",
+      lastName: "18680506199203100",
+    },
+    address: "Farah Nadia Putri",
+    city: "Camat",
+    state: "Pembina/IV A",
+    nosp: "209/09/2022",
+    pensiun: "09/09/2024",
+    kerja: "PLT",
+  },
+  {
+    name: {
+      firstName: "098579",
+      lastName: "18680506199203100",
+    },
+    address: "Farah Nadia Putri",
+    city: "Camat",
+    state: "Pembina/IV A",
+    nosp: "209/09/2022",
+    pensiun: "09/09/2024",
+    kerja: "PLT",
+  },
+  {
+    name: {
+      firstName: "098579",
+      lastName: "18680506199203100",
+    },
+    address: "Farah Nadia Putri",
+    city: "Camat",
+    state: "Pembina/IV A",
+    nosp: "209/09/2022",
+    pensiun: "09/09/2024",
+    kerja: "PLT",
+  },
+  {
+    name: {
+      firstName: "098579",
+      lastName: "18680506199203100",
+    },
+    address: "Farah Nadia Putri",
+    city: "Camat",
+    state: "Pembina/IV A",
+    nosp: "209/09/2022",
+    pensiun: "09/09/2024",
+    kerja: "PLT",
+  },
+  {
+    name: {
+      firstName: "098579",
+      lastName: "18680506199203100",
+    },
+    address: "Farah Nadia Putri",
+    city: "Camat",
+    state: "Pembina/IV A",
+    nosp: "209/09/2022",
+    pensiun: "09/09/2024",
+    kerja: "PLT",
+  },
+];
+
+const tableSurat = () => {
+  //should be memoized or stable
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(() => {
+    return [
+      {
+        accessorKey: "index", // new accessor for row number
+        header: "No",
+        size: 50,
+        Cell: ({ row }) => row.index + 1, // display the row index + 1
+      },
+      {
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "NRK",
+        size: 150,
+      },
+      {
+        accessorKey: "name.lastName",
+        header: "NIP",
+        size: 150,
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Nama Lengkap",
+        size: 200,
+      },
+      {
+        accessorKey: "city",
+        header: "Jabatan",
+        size: 150,
+      },
+      {
+        accessorKey: "state",
+        header: "Pangkat/Gol",
+        size: 150,
+      },
+      {
+        accessorKey: "nosp",
+        header: "No.SP",
+        size: 150,
+      },
+      {
+        accessorKey: "pensiun",
+        header: "Tanggal SP",
+        size: 150,
+      },
+      {
+        accessorKey: "kerja",
+        header: "Jenis Pengajuan",
+        size: 150,
+      },
+      columnHelper.display({
+        id: "actions",
+        size: 10,
+        header: "Actions",
+        Cell: ({ row }: { row: MRT_Row<Person> }) => (
+          <Box sx={{ display: "flex" }}>
+            <Button>
+              <EditIcon fontSize="small" />
+            </Button>
+          </Box>
+        ),
+      }),
+    ];
+  }, []);
+
+  // Create a new data array with index property
+  const dataWithIndex = data.map((item, index) => ({ index, ...item }));
+
+  const table = useMaterialReactTable({
+    columns,
+    data: dataWithIndex, // use data with index
+  });
+
+  return <MaterialReactTable table={table} />;
+};
+
+export default tableSurat;
---
+++ src/components/Tables/makeData.tsx
@@ -0,0 +1,67 @@
+export type Person = {
+  id: number;
+  tanggal: string;
+  firstName: string;
+  lastName: string;
+  company: string;
+  city: string;
+  country: string;
+};
+
+export const data: Person[] = [
+  {
+    id: 1,
+    tanggal: "DD-MM-YYYY",
+    firstName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    lastName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    company: "DD-MM-YYYY sd  DD-MM-YYYY",
+    city: "Verifikasi : DD-MM-YYYY Validasi : DD-MM-YYYY",
+    country: "Verifikasi BKD/Suban",
+  },
+  {
+    id: 2,
+    tanggal: "DD-MM-YYYY",
+    firstName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    lastName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    company: "DD-MM-YYYY sd  DD-MM-YYYY",
+    city: "Verifikasi : DD-MM-YYYY Validasi : DD-MM-YYYY",
+    country: "Verifikasi BKD/Suban",
+  },
+  {
+    id: 3,
+    tanggal: "DD-MM-YYYY",
+    firstName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    lastName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    company: "DD-MM-YYYY sd  DD-MM-YYYY",
+    city: "Verifikasi : DD-MM-YYYY Validasi : DD-MM-YYYY",
+    country: "Verifikasi BKD/Suban",
+  },
+  {
+    id: 4,
+    tanggal: "DD-MM-YYYY",
+    firstName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    lastName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    company: "DD-MM-YYYY sd  DD-MM-YYYY",
+    city: "Verifikasi : DD-MM-YYYY Validasi : DD-MM-YYYY",
+    country: "Verifikasi BKD/Suban",
+  },
+  {
+    id: 5,
+    tanggal: "DD-MM-YYYY",
+    firstName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    lastName:
+      "Nama : Ridwan Yusuf Kamil Jabatan : Sekretariat Presiden NIP/NRK : 28738271983798 Pangkat/Gol : Jendral V",
+    company: "DD-MM-YYYY sd  DD-MM-YYYY",
+    city: "Verifikasi : DD-MM-YYYY Validasi : DD-MM-YYYY",
+    country: "Verifikasi BKD/Suban",
+  },
+];
