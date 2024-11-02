e40b89f89ba39a4f6edb900b90d72214b9b095fc
Rafi Putra Sudirman <rafikujra@gmail.com>
2024-11-02 20:30:09 +0700

    penambahan plt plh input pengajuan

---
+++ src/app/plh/input/tambah/page.tsx
@@ -0,0 +1,19 @@
+import InputPengajuan from "@/components/Plh/input_pengajuan";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/Breadcrumbs/BreadcrumbPlh";
+
+export const metadata: Metadata = {
+  title: "PLH | Input Pengajuan",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <Breadcrumb pageName="Input Pengajuan" />
+        <InputPengajuan />
+      </DefaultLayout>
+    </>
+  );
+}
---
+++ src/app/plt/input/tambah/page.tsx
@@ -0,0 +1,20 @@
+import PLH from "@/components/Plh/Plh";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/Breadcrumbs/BreadcrumbPlt";
+import InputPengajuan from "@/components/Plt/input_pengajuan";
+
+export const metadata: Metadata = {
+  title: "PLT | Input Pengajuan",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <Breadcrumb pageName="Input Pengajuan" />
+        <InputPengajuan />
+      </DefaultLayout>
+    </>
+  );
+}
